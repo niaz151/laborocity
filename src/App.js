5d2e537c1e7be2b41290e73c9006242582678309
niaz151 <niaz151@gmail.com>
2019-10-19 13:52:31 -0400

    Add Header Divs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="border border-primary App">
-      App
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return(
+    <div className='position-absolute w-100 header'>
+      Header
+    </div>
+  )
+}
+
+export default Header;
