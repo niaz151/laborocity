51e35bcd4fc833fabd52e789afef9d8942048af2
niaz151 <niaz151@gmail.com>
2019-10-20 13:35:28 -0400

    Add Fluid Typography and Human SVG

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import './styles/App.css';
+import Tabs from './components/Tabs';
 
 function App() {
   return (
     <div className='app-container'>
       <Header/>
+      <div className='app-body'>
+        <Tabs/>
+        <img src={require('./images/person.svg')} />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/Header.css';
 
+
 const Header = () => {
   return(
     <div className='container-fluid position-relative d-flex align-items-center w-100 header'>
-      <img className='pl-4 position-absolute company-icon' src={require('../images/logo.svg')} />
-      <img className='pr-4 position-absolute menu-icon' src={require('../images/menu.svg')} />
+      <img className='pl-3 position-absolute company-icon' src={require('../images/logo.svg')} />
+      <img className='pr-3 position-absolute menu-icon' src={require('../images/menu.svg')} />
     </div>
   )
 }
