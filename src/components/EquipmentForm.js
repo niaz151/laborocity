746bafe0267d938748ffec784945b277c51725b9
niaz151 <niaz151@gmail.com>
2019-10-22 02:00:46 -0400

    Minor CSS Edits

--- src/components/EquipmentForm.js
+++ src/components/EquipmentForm.js
@@ -2,35 +2,35 @@ import React from 'react';
 import {reduxForm, Field} from 'redux-form';
 import {connect} from 'react-redux';
 import Switch from './Switch';
+import '../styles/EquipmentForm.css';
 
-
-class LaborForm extends React.Component{
+class EquipmentForm extends React.Component{
 
   render(){
     return(
       <form className='d-flex flex-column align-items-center justify-content-around category-form-container'>
         
-        <div className='position-relative d-flex align-items-center category-form-row'>
+        <div className=''>
           <label className='position-absolute ml-4 category-form-label'> Steel Toed Boots</label>
           <Field name='steel_toed_boots' component={Switch} />
         </div>
 
-        <div className='position-relative d-flex align-items-center category-form-row'>
+        <div className=''>
           <label className='position-absolute ml-4 category-form-label'> Hard Hat </label>
           <Field name='hard_hat' component={Switch} />
         </div>
 
-        <div className='position-relative d-flex align-items-center category-form-row'>
+        <div className=''>
           <label className='position-absolute ml-4 category-form-label'> Safety Vest </label>
           <Field name='safety_vest' component={Switch} />
         </div>
 
-        <div className='position-relative d-flex align-items-center category-form-row'>
+        <div className=''>
           <label className='position-absolute ml-4 category-form-label'> Eye Protection </label>
           <Field name='eye_protection' component={Switch} />
         </div>
 
-        <div className='position-relative d-flex align-items-center category-form-row'>
+        <div className=''>
           <label className='position-absolute ml-4 category-form-label'> Gloves </label>
           <Field name='gloves' component={Switch} />
         </div>
@@ -51,4 +51,4 @@ function mapStateToProps(state){
 
 export default connect(mapStateToProps,null)(reduxForm({
   form:'category-form'
-})(CategoryForm))
+})(EquipmentForm))
